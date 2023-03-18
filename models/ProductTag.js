7deb3155cf8e499cc7b37e93f2a1f050cb632866
Claire Bain <claire.bain3@gmail.com>
2023-03-18 13:52:40 -0700

    sets up models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,6 +6,31 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "Product",
+        key: "id",
+        unique: false,
+      },
+   },
+   tag_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "Tag",
+      key: "id",
+      unique: false,
+    },
+  
+    
+    },
+
     // define columns
   },
   {
