d1d4e03aff551b4fcee266f7ff438b311163a4ba
Claire Bain <claire.bain3@gmail.com>
2023-03-18 14:05:52 -0700

    sets up model associations

--- models/index.js
+++ models/index.js
@@ -6,11 +6,38 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+  // Define the third table needed to store the foreign keys
+  foreignkey: "category_id",
+});
+
 // Categories have many Products
+Category.belongsToMany(Product, {
+  foreignKey: "category_id",
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'product_tags'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'tag_products'
+});
+
 
 module.exports = {
   Product,
